ac4c84d8f0a187f53400dfd16eca98e912061c87
tymofiichuy <tymofiichuy@gmail.com>
2024-09-18 22:32:39 +0300

    Add long_add and long_sub

--- src/long_arithmetic.cpp
+++ src/long_arithmetic.cpp
@@ -1,6 +1,8 @@
 #include "long_arithmetic.hpp"
-
 #include<intrin.h>
+
+using namespace std;
+uint64_t base = 1<<64;
 
 long_int::long_int(){
     this->digits = new uint64_t[32];
@@ -11,4 +13,28 @@ long_int::long_int(){
 
 long_int::~long_int(){
     delete[] digits;
+}
+
+unsigned char long_add(long_int& in1, long_int& in2, unsigned char carry_bit = 0, long_int& out){
+    for(int i = 0; i < 32; i++){
+        carry_bit = _addcarry_u64(carry_bit, in1.digits[i], in2.digits[i], &out.digits[i]);
+    }
+    return carry_bit;
+}
+
+//problem: uint is always positive
+unsigned char long_sub(long_int& in1, long_int& in2, unsigned char borrow_bit = 0, long_int& out){
+    uint64_t temp;
+    for(int i = 0; i < 32; i++){
+        temp = in1.digits[i] - in2.digits[i] - borrow_bit;
+        if(temp >= 0){
+            out.digits[i] = temp;
+            borrow_bit = 0;
+        }
+        else{
+            out.digits[i] = temp + base;
+            borrow_bit = 1;
+        }
+    }
+    return borrow_bit;
 }
--- src/long_arithmetic.hpp
+++ src/long_arithmetic.hpp
@@ -6,7 +6,7 @@ class long_int{
 public:
     //!
     uint64_t* digits;
-    // bool sign = true;
+    // unsigned char sign = true;
 
     void operator>>(int step);
     void operator<<(int step);
@@ -16,8 +16,10 @@ public:
 };
 
 //next functions return is a new carry/borrow bit
-bool long_add(long_int& in1, long_int& in2, bool carry_bit, long_int& out);
-bool long_sub(long_int& in1, long_int& in2, bool borrow_bit, long_int& out);
+unsigned char long_add(long_int&, long_int&, unsigned char, long_int&);
+//unsigned char long_add(long_int& in1, long_int& in2, unsigned char carry_bit = 0, long_int& out);
+unsigned char long_sub(long_int&, long_int&, unsigned char, long_int&);
+//unsigned char long_sub(long_int& in1, long_int& in2, unsigned char borrow_bit = 0, long_int& out);
 //here must be comparison declaration
 void long_multiply_by_one_digit(long_int& long_in, uint64_t digit_in, long_int& out);
 void long_multiply(long_int& in1, long_int& in2, long_int& out);
