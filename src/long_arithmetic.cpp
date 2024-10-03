ae2dcc57d9c410da54eda3963dfe6fab56ea2f2c
tymofiichuy <tymofiichuy@gmail.com>
2024-10-03 22:21:34 +0300

    Add multiplication

--- src/long_arithmetic.cpp
+++ src/long_arithmetic.cpp
@@ -1,49 +1,19 @@
 #include "long_arithmetic.hpp"
 #include<intrin.h>
+#include <iostream>
 
 using namespace std;
 //uint64_t base = 1<<64;
 
-long_int::long_int(){
-    this->digits = new digit[32];
-    for(int i = 0; i < 32; i++){
-        this->digits[i].value = 0;
-    }
-}
-
-void long_int::reset(){
-    for(int i = 0; i < 32; i++){
-        this->digits[i].value = 0;
-    } 
-}
-
-long_int::~long_int(){
-    delete[] digits;
-}
-
-//use subtraction instead?
-bool long_int::operator==(long_int& in){
-    for(int i = 0; i < 32; i++){
-        if(this->digits[i].value != in.digits[i].value){
-            return false;
-        }
-    }
-    return true;
-}
-
-digit& long_int::operator[](int i){
-    return this->digits[i];
-}
-
 //can be optimized (if needed) by reducing tmp size to 64 bits and using flag to capture overflow
-unsigned char long_add(long_int& in1, long_int& in2, unsigned char carry_bit = 0, long_int& out){
+unsigned char long_add(long_int& in1, long_int& in2, long_int& out, unsigned char carry_bit = 0){
     for(int i = 0; i < 32; i++){
         carry_bit = _addcarry_u64(carry_bit, in1[i].value, in2[i].value, &out[i].value);
     }
     return carry_bit;
 }
 //can be optimized (if needed) by reducing tmp size to 64 bits and using flag to capture overflow
-unsigned char long_sub(long_int& in1, long_int& in2, unsigned char borrow_bit = 0, long_int& out){
+unsigned char long_sub(long_int& in1, long_int& in2, long_int& out, unsigned char borrow_bit = 0){
     for(int i = 0; i < 32; i++){
         borrow_bit = _subborrow_u64(borrow_bit, in1[i].value, in2[i].value, &out[i].value);
     }    
@@ -62,9 +32,48 @@ void long_multiply_by_one_digit(long_int& long_in, digit digit_in, long_int& car
         }
         delete[] temp;
     }
-    long_add(out, carry, 0, out);
+    long_add(out, carry, out);
 }
 
 void long_sub_multiply(long_int& in1, long_int& in2, long_int& out){
-    
+    out.reset();
+    long_int temp;
+    long_int carry;
+    for(int i = 0; i < 32; i++){
+        carry.reset();
+        long_multiply_by_one_digit(in1, in2[i], carry, temp);
+        temp.long_upper_super_shift(i);
+        long_add(out, temp, out);
+    }
 }
+
+// void long_int::split(long_int* out, int iter){
+//     out[0].reset();
+//     out[1].reset();
+//     int upper = 32<<iter;
+//     int lower = 16<<iter;
+
+//     for(int i = 0; i < lower; i++){
+//         out[0][i] = this->digits[i];
+//     }
+
+//     for(int i = lower; i < upper; i++){
+//         out[1][i-lower] = this->digits[i];
+//     }
+// }
+
+// void long_super_multiply(long_int& in1, long_int& in2, long_int& out, int iter){
+//     long_int split_1[2];
+//     long_int split_2[2];
+//     in1.split(split_1, iter);
+//     in2.split(split_2, iter);
+
+// }
+
+// void long_int::split(long_int* out){
+//     for(int i = 0; i < 8; i++){
+//         for (int j = 0; j < 4; j++){
+//             out[i][j] = this->digits[i*4+j];
+//         }
+//     }
+// }
---
+++ src/operators.cpp
@@ -0,0 +1,72 @@
+#include "long_arithmetic.hpp"
+#include <iostream>
+
+using namespace std;
+
+long_int::long_int(){
+    this->digits = new digit[32];
+    for(int i = 0; i < 32; i++){
+        this->digits[i].value = 0;
+    }
+}
+
+void long_int::reset(){
+    for(int i = 0; i < 32; i++){
+        this->digits[i].value = 0;
+    } 
+}
+
+long_int::~long_int(){
+    delete[] digits;
+}
+
+//use subtraction instead?
+bool long_int::operator==(long_int& in){
+    for(int i = 0; i < 32; i++){
+        if(this->digits[i].value != in.digits[i].value){
+            return false;
+        }
+    }
+    return true;
+}
+
+digit& long_int::operator[](int i){
+    return this->digits[i];
+}
+
+void long_int::long_upper_super_shift(int shift){
+    if(shift>32 || shift<0){
+        throw invalid_argument("Attempt to shift an invalid number of digits");
+    }
+    else{
+        for(int i = 0; i < 32-shift; i++){
+            digits[i+shift].value = digits[i].value;
+        }  
+        for(int i = 0; i < shift; i++){
+            digits[i].value = 0; 
+        } 
+    }
+}
+
+void long_int::long_upper_sub_shift(int shift){
+    if(shift>64 || shift<0){
+        throw invalid_argument("Attempt to shift an invalid number of bits");
+    }
+    else{
+        //uint64_t mask = ~0ULL<<64-shift;
+        uint64_t carry_in = 0;
+        uint64_t carry_out = 0;
+        for(int i = 0; i<32; i++){
+            carry_out = this->digits[i].value>>(64-shift);
+            this->digits[i].value = (this->digits[i].value<<shift)|carry_in;
+            carry_in = carry_out;
+        }
+    }
+}
+
+void long_int::operator<<(int shift){
+    int super_shift = shift/64;
+    int sub_shift = shift%64;
+    this->long_upper_super_shift(super_shift);
+    this->long_upper_sub_shift(sub_shift);
+}
